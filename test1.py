5ae518b5726431f9f81d508a6b17f3f08bfc99b2
Tim Aarons <Tim.Aarons@gmail.com>
2020-11-30 05:47:03 -0800

    adding a couple of python files

---
+++ test1.py
@@ -0,0 +1,5 @@
+
+print("totally new file pynetA - File test 1")
+
+print("totally new file pynetA - File test 1")
+print("totally new file pynetA - File test 1")
---
+++ test2.py
@@ -0,0 +1,5 @@
+print("something a little different...")
+print("totally new file pynetA - File test 1")
+
+print("totally new file pynetA - File test 1")
+print("totally new file pynetA - File test 1")
